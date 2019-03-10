a8dd9ebf875138d24842ff9345ca367acdf8b34f
zhoufan22 <47655785+zhoufan22@users.noreply.github.com>
2019-03-10 07:42:27 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* recomended 40 sec */
+$set_delay=40;
+
+$config=[
+"imei" => "866657037761707",
+"sign" => "57283ef20ee5f7f135a8589a80e4afe0cdf6654b",
+"uuid" => "d584d04b47817ffb",
+"token" => "a458801f7a2809a4d05c04a90c160ae9"
+];
